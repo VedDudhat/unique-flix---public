df4b4444fdcf901492987e493b9023c25aedecbd
Ved Dudhat <veddudhat18@gmail.com>
2026-05-11 13:28:00 +0530

    removed pymysql and url updated

--- backend/database/database.py
+++ backend/database/database.py
@@ -2,7 +2,7 @@ from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker,declarative_base
 import os
 
-DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./uniqueflix.db")
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql+asyncpg://neondb_owner:npg_DV24dMgJqzaR@ep-icy-dew-ao8xhqbu-pooler.c-2.ap-southeast-1.aws.neon.tech/neondb?ssl=require")
 
 engine = create_async_engine(
     DATABASE_URL,
